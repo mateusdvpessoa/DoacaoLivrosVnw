d9bc684a9dfcfa09675c14bb815be091542b5ffa
mateusdvpessoa <mateusdavi.07@gmail.com>
2024-12-11 17:21:19 -0300

    :sparkles: feat: Adicionando arquivos de componente de home e formulário de doação

---
+++ src/Pages/DonateForm/DonateForm.jsx
@@ -0,0 +1,24 @@
+import S from './donateform.module.scss'
+import livro from '../../../assets/Vector.png'
+
+
+export default function DonateForm(){
+    return(
+        <section className={S.principal}>
+            <section className={S.container}>
+                <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
+                <form action="">
+                    <div>
+                        <img src={livro} alt="" />
+                        <h3>Informações do Livro</h3>
+                    </div>
+                    <input type="text" placeholder='Titulo' />
+                    <input type="text" placeholder='Categoria' />
+                    <input type="text" placeholder='Autor' />
+                    <input type="text" placeholder='Link da Imagem' />
+                    <input className={S.doar} type="submit" value="Doar" />
+                </form>
+            </section>
+        </section>
+    )
+}
---
+++ src/Pages/DonateList/DonateList.jsx
@@ -0,0 +1,36 @@
+import S from './donatelist.module.scss'
+import bookCard from '../../../assets/book-card.png'
+
+export default function DonateList(){
+    return(
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxBooks}>
+                <article>
+                    <img src={bookCard} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={bookCard} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={bookCard} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={bookCard} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
+        </section>
+    )
+}
