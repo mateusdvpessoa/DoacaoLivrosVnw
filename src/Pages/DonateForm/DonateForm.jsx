08beafb6926590fac1b26a2ad92b789dbd2a3507
mateusdvpessoa <mateusdavi.07@gmail.com>
2025-04-04 09:13:44 -0300

    :spartkles: feat: adicionando métodos de integração de api e importando axios

--- src/Pages/DonateForm/DonateForm.jsx
+++ src/Pages/DonateForm/DonateForm.jsx
@@ -1,8 +1,56 @@
 import S from './donateform.module.scss'
 import livro from '../../../assets/Vector.png'
+import axios from 'axios' 
+import {useState} from 'react'
 
 
 export default function DonateForm(){
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+    const enviarDados = async() => {
+
+            const urlApi = "https://apidoacaolivrosvainaweb.onrender.com/doar" 
+    
+            const dadosEnviar = {
+                titulo,
+                categoria,
+                autor,
+                imagem_url
+            }
+    
+            await axios.post(urlApi, dadosEnviar) 
+    
+            alert("Livro cadastrado com sucesso!")
+    
+            setTitulo("")
+            setCategoria("")
+            setAutor("")
+            setImagem_url("")
+
+            console.log("Livro Cadastrado com sucesso!")
+        }
+    
+        const capturaTitulo = (e) =>{
+            setTitulo(e.target.value)
+        }
+    
+        const capturaCategoria = (e) =>{
+            setCategoria(e.target.value)
+        }
+    
+        const capturaAutor = (e) =>{
+            setAutor(e.target.value)
+        }
+    
+        const capturaImagem = (e) =>{
+            setImagem_url(e.target.value)
+        }
+    
+
     return(
         <section className={S.principal}>
             <section className={S.container}>
@@ -12,11 +60,11 @@ export default function DonateForm(){
                         <img src={livro} alt="" />
                         <h3>Informações do Livro</h3>
                     </div>
-                    <input type="text" placeholder='Titulo' />
-                    <input type="text" placeholder='Categoria' />
-                    <input type="text" placeholder='Autor' />
-                    <input type="text" placeholder='Link da Imagem' />
-                    <input className={S.doar} type="submit" value="Doar" />
+                    <input type="text" placeholder='Titulo' onChange={capturaTitulo} />
+                    <input type="text" placeholder='Categoria' onChange={capturaCategoria} />
+                    <input type="text" placeholder='Autor' onChange={capturaAutor} />
+                    <input type="text" placeholder='Link da Imagem' onChange={capturaImagem} />
+                    <input className={S.doar} type="submit" value="Doar" onClick={enviarDados}/>
                 </form>
             </section>
         </section>
