3d8ba6839600e3293de26f0ef8315f49d22e398a
mateusdvpessoa <mateusdavi.07@gmail.com>
2025-04-04 09:27:21 -0300

    :sparkles: feat: configurando método de busca de dados da api de livros

--- src/Pages/DonateList/DonateList.jsx
+++ src/Pages/DonateList/DonateList.jsx
@@ -1,35 +1,36 @@
 import S from './donatelist.module.scss'
-import bookCard from '../../../assets/book-card.png'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-export default function DonateList(){
+export default function DonateList() {
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        try {
+            const response = await axios.get("https://apidoacaolivrosvainaweb.onrender.com/livros")
+            setLivros(response.data)
+        } catch (error) {
+            console.error("Erro ao buscar livros:", error)
+        }
+    }
+
+    useEffect(() => {
+        getLivros()
+    }, [])
+
     return(
         <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
             <section className={S.boxBooks}>
-                <article>
-                    <img src={bookCard} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={bookCard} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={bookCard} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={bookCard} alt="imagem do livro o protagonista" />
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
+                {livros.map((item) => (
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt={item.titulo} />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.autor}</p>
+                        <p>{item.categoria}</p>
+                    </article>
+                ))}
             </section>
         </section>
     )
